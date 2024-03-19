48c97f1d5e053fe84721088710d995fd8079d40c
baldockb2514 <111828862+baldockb2514@users.noreply.github.com>
2024-03-20 12:26:08 +1300

    Converts units

---
+++ C_02_Unit_Converter_v1.py
@@ -0,0 +1,125 @@
+# Functions go here
+import re
+
+
+# Converts units
+def unit_converter(unit, amount):
+
+    # dict of units and what to multiply the
+    # original amount by convert to ideal unit
+    unit_dict = {
+        "mg": 0.001,
+        "g": 1,
+        "kg": 1000,
+        "ml": 0.001,
+        "l": 1,
+        "kl": 1000
+    }
+
+    if unit in ["mg", "g", "kg"]:
+        final_unit = "g"
+    else:
+        final_unit = "l"
+    converted = amount * float(f"{unit_dict.get(unit)}")
+    return f"{converted}{final_unit}"
+
+
+# checks user response is a valid response based on a list of options
+def string_check(question, answer_list, short_response, error):
+    valid = False
+    while not valid:
+        # make response lowercase and get rid of spaces
+        response = input(question).lower().replace(" ", "")
+
+        while True:
+            for item in answer_list:
+                try:
+                    short_response = int(short_response)
+                    if response == item[:short_response] or response == item:
+                        return item
+
+                except TypeError:
+                    list_spot = answer_list.index(item)
+                    if response == short_response[list_spot] or response == item:
+                        return item
+
+            # If not, print error
+            else:
+                print(error)
+                print()
+                break
+
+
+while True:
+    while True:
+
+        # get the original item (amount + unit or just amount)
+        print()
+        get_item = input("Please enter the weight/amount: ").replace(" ", "").replace(",", "")
+        if get_item == "" or get_item[0] == "-":
+            print("Please enter an amount more than 0")
+            continue
+        elif get_item == "xxx":
+            break
+
+        # if they didn't input a unit, ask for unit
+        try:
+            get_amount = float(get_item)
+            get_unit = string_check("Weight unit? ", ["g", "mg", "kg", "xxx", "ml", "l", "kl"],
+                                    ["gram", "milligram", "kilogram", "xxx", "millilitres", "litres", "kilolitres"],
+                                    "Please answer mg/g/kg or xxx to quit.")
+
+            if get_unit == "xxx":
+                break
+
+        # otherwise,separate amount from unit
+        except ValueError:
+
+            # separate amount from item
+            get_amount = re.findall(r'[0-9]+', get_item)
+
+            # if there is no amount, output error
+            if len(get_amount) == 0:
+                print("Please enter an amount more than 0")
+                continue
+
+            # if the list is longer than one, format as a decimal
+            if len(get_amount) == 2:
+                get_amount = f"{get_amount[0]}.{get_amount[1]}"
+
+            # else, put the list into a string to convert to float
+            else:
+                get_amount = f"{get_amount[0]}"
+
+            get_amount = float(get_amount)
+
+            # if the amount is 0, output error
+            if get_amount == 0:
+                print("Please enter an amount more than 0")
+                continue
+
+            # Get unit from item and turn to string
+            get_unit = re.findall(r'[a-zA-Z]+', get_item)
+            get_unit = f"{get_unit[0]}"
+            get_unit = get_unit.replace("s", "")
+            # if their unit is kilo, ask for the user to specify
+            if get_unit == "kilo":
+                get_unit = string_check("do you mean kilograms or kilolitres?: ",
+                                        ["kilogram", "kilolitre", "kilograms", "kilolitres", "xxx"],
+                                        ["kg", "kl", "kg", "kl", "xxx"], "Please enter either kg or kl")
+
+            if len(get_unit) > 2:
+                get_unit = get_unit.replace("s", "")
+                get_unit_long = ["milligram", "gram", "kilogram", "millilitre", "litre", "kilolitre", "xxx"]
+                get_unit_short = ["mg", "g", "kg", "ml", "l", "kl", "xxx"]
+                list_location = get_unit_long.index(get_unit)
+                get_unit = get_unit_short[list_location]
+
+            if get_unit == "xxx":
+                break
+
+        # us function to convert and print
+        converted_item = unit_converter(get_unit, get_amount)
+        print(converted_item)
+
+    print("Loop Breaks")
