107599256e55055a0e849deae19505cb2cfb9740
baldockb2514 <111828862+baldockb2514@users.noreply.github.com>
2024-03-22 14:58:23 +1300

    Checks if a input is a float or 'xxx'
    Changed to only check floats
    Let 'xxx' be a valid input

---
+++ C_03_Float_check_v2.py
@@ -0,0 +1,51 @@
+# Check that an int/float is more than 0
+def num_check(question, error, allow_blank):
+    while True:
+
+        response = input(question).replace(" ", "")
+        try:
+            response = float(response)
+
+            if response <= 0:
+                print(error)
+                continue
+
+            return response
+
+        except ValueError:
+            if response == "xxx":
+                return response
+            elif allow_blank == "no" or response != "":
+                print(error)
+                continue
+            else:
+                return response
+
+
+# Main routine goes here
+while True:
+
+    while True:
+        # Ask the user for their budget
+        get_budget = num_check("\nWhat is your budget?(press <enter> if you have no budget): $",
+                               "Please enter a number more than 0 (or <enter> for no budget)", "yes")
+        # if the user doesn't enter a budget, assume they have no budget
+        if get_budget == "":
+            print("No Budget (Program continues)")
+            continue
+        elif get_budget == "xxx":
+            print("Loop ends")
+            break
+        else:
+            print(f'${get_budget:.2f} (Program continues)')
+            continue
+
+    while True:
+
+        get_cost = num_check("\nHow much does it cost/ $", "Please enter a number more than 0", "no")
+        if get_cost == "xxx":
+            print("Loop ends\n********************")
+            break
+        else:
+            print(f'${get_cost:.2f} (Program continues)')
+            continue
